5a8431925d1dd923a4f63f63bc06d34d9aa1d126
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-03-27 14:41:15 -0500

    Added shaders.

--- main.cpp
+++ main.cpp
@@ -85,6 +85,36 @@ int main(int argc, char* argv[])
 
 	buffer_clear(&buffer, clear_color);
 
+	GLuint fullscreen_triangle_vao;
+	glGenVertexArrays(1, &fullscreen_triangle_vao);
+	glBindVertexArray(fullscreen_triangle_vao);
+
+	static const char* vertex_shader =
+		"\n"
+		"#version 330\n"
+		"\n"
+		"noperspective out vec2 TexCoord;\n"
+		"void main(void){\n"
+		"\n"
+		"	TexCoord.x = (gl_VertexID == 2)? 2.0: 0.0;\n"
+		"	TexCoord.y = (gl_VertexID == 1)? 2.0: 0.0;\n"
+		"	\n"
+		"	gl_Position = vec4(2.0 * TexCoord - 1.0, 0.0, 1.0);\n"
+		"}\n";
+
+	static const char* fragment_shader =
+		"\n"
+		"#version 330\n"
+		"\n"
+		"uniform sampler2D buffer;\n"
+		"noperspective in vec2 TexCoord;\n"
+		"\n"
+		"out vec3 outColor;\n"
+		"\n"
+		"void main(void){\n"
+		"	outColor = texture(buffer, TexCoord).rgb;\n"
+		"}\n";
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);

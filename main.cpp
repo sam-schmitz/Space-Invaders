de4cb3ac20dfa95ee3efaabe9e2172e9db6e0291
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-03-27 15:12:02 -0500

    added textures to the buffer.

--- main.cpp
+++ main.cpp
@@ -117,6 +117,20 @@ int main(int argc, char* argv[])
 
 	buffer_clear(&buffer, clear_color);
 
+	GLuint buffer_texture;
+	glGenTextures(1, &buffer_texture);
+
+	glBindTexture(GL_TEXTURE_2D, buffer_texture);
+	glTexImage2D(
+		GL_TEXTURE_2D, 0, GL_RGB8,
+		buffer.width, buffer.height, 0,
+		GL_RGBA, GLUNSIGNED_INT_8_8_8_8, buffer.data
+	);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
 	GLuint fullscreen_triangle_vao;
 	glGenVertexArrays(1, &fullscreen_triangle_vao);
 	glBindVertexArray(fullscreen_triangle_vao);
@@ -182,6 +196,9 @@ int main(int argc, char* argv[])
 		delete[] buffer.data;
 		return -1;
 	}
+
+	GLint location = glGetUniformLocation(shader_id, "buffer");
+	glUniform1i(location, 0);
 
 	while (!glfwWindowShouldClose(window))
 	{

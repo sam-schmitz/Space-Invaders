b321a2e4180969644d63818e3e5a39784de2672e
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-03-27 19:26:09 -0500

    Added the abilily to close the game with <esc>

--- main.cpp
+++ main.cpp
@@ -7,9 +7,32 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+bool game_running = true;
+
 void error_callback(int error, const char* description)
 {
 	fprintf(stderr, "Error: %s\n", description);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	switch (key) {
+	case GLFW_KEY_ESCAPE:
+		if (action == GLFW_PRESS) game_running = false;
+		break;
+	//case GLFW_KEY_RIGHT:
+		//if (action == GLFW_PRESS) move_dir += 1;
+		//else if (action == GLFW_RELEASE) move_dir -= 1;
+		//break;
+	//case GLFW_KEY_LEFT:
+		//if (action == GLFW_PRESS) move_dir -= 1;
+		//else if (action == GLFW_RELEASE) move_dir += 1;
+		//break;
+	//case GLFW_KEY_SPACE:
+		//if (action == GLFW_RELEASE) fire_pressed = true;
+		//break;
+	default:
+		break;
+	}
 }
 
 void validate_shader(GLuint shader, const char* file = nullptr)
@@ -140,6 +163,9 @@ int main(int argc, char* argv[])
 		glfwTerminate();
 		return -1;
 	}
+
+	glfwSetKeyCallback(window, key_callback);
+
 	glfwMakeContextCurrent(window);
 
 	GLenum err = glewInit();
@@ -343,7 +369,7 @@ int main(int argc, char* argv[])
 	uint32_t clear_color = rgb_to_uint32(0, 128, 0);
 
 	int player_move_dir = 1;
-	while (!glfwWindowShouldClose(window))
+	while (!glfwWindowShouldClose(window) && game_running)
 	{
 		buffer_clear(&buffer, clear_color);
 
